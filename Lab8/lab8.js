e829a3d59aba2c4477e2993a2e61a6f1c5d1d7c7
Keenan <116980114+KayoCodes@users.noreply.github.com>
2023-10-20 16:00:04 -0500

    Test

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -3,6 +3,17 @@ function getDataFromForm() {
     var second = document.getElementsByName("lname")[0].value;
 
     alert(first.value );
+}
 
 
-  }
+function runAjax(fname, lname){
+    var xhr = new XMLHttpRequest;
+    xhr.onload = function(){
+        if (xhr.status === 200){
+            this.response = fname+lname;
+            document.getElementById("responseString").innerHTML = this.response
+        }
+    }
+    xhr.open('GET','ajax.php',true)
+    xhr.send()
+}

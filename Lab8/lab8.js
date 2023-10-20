b2ba24159dd97dcf033fd603baa5cee47eae3a70
Keenan <116980114+KayoCodes@users.noreply.github.com>
2023-10-20 16:06:59 -0500

    Test

--- Lab8/lab8.js
+++ Lab8/lab8.js
@@ -2,7 +2,7 @@ function getDataFromForm() {
     var first = document.getElementsByName("fname")[0].value;
     var second = document.getElementsByName("lname")[0].value;
 
-    alert(first.value );
+    runAjax(first.value, second.value);
 }
 
 

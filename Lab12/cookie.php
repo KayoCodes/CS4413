df9b0973a2111f4befb243853da57070c647f38f
Keenan <116980114+KayoCodes@users.noreply.github.com>
2023-11-11 09:11:46 -0800

    New change

--- Lab12/cookie.php
+++ Lab12/cookie.php
@@ -1,28 +1,19 @@
-<!DOCTYPE html>
 <?php
-$cookie_name ="cs4413";
-$cookie_value = "This is My Cs4413 Cookie!";
-setcookie($cookie_name, $cookie_value, time() + 4000);
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 ?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab12</title>
-</head>
+<html>
 <body>
-    <p>Lab 12</p>
 
- <?php
-
-if (isset($_COOKIE[$cookie_name])){
-    echo "Cookie named ". $cookie_name . " is set" ;
-}else{
-echo "Cookie is not set";
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+  echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+  echo "Cookie '" . $cookie_name . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$cookie_name];
 }
-
 ?>
-    
+
 </body>
 </html>
-
